12ab09a9c15b3cb3f78a5452f0c1da1b7e0019f2
safavimohadeseh78 <safavimohadeseh78@gmail.com>
2025-06-10 22:05:05 +0330

    Part3

---
+++ main/mainPage/components/part3.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="services-one services-one--services">
+    <div class="container">
+        <div class="row">
+            <!--Start Services One Single -->
+            <div class="col-xl-4 col-lg-4 wow animated fadeInUp" data-wow-delay="0.1s">
+                <div class="services-one__single">
+                    <div class="services-one__single-title">
+                        <div class="icon">
+                            <span class="icon-net-connect-plus"></span>
+                        </div>
+                        <h3><a href="ai-lnsights.html">نت کانکت پلاس</a></h3>
+                    </div>
+                    <div class="services-one__single-content">
+                        <p>این یک واقعیت ثابت شده است که یک خواننده ممکن است با محتوای قابل خواندن یک صفحه، که به طور طولانی تثبیت شده است، حواسش پرت شود.</p>
+                        <div class="btn-box">
+                            <a class="button-style-2" href="ai-lnsights.html">
+                                بیشتر بخوانید
+                                <i class="icon-plus-1"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--End Services One Single -->
+            <!--Start Services One Single -->
+            <div class="col-xl-4 col-lg-4 wow animated fadeInUp" data-wow-delay="0.3s">
+                <div class="services-one__single">
+                    <div class="services-one__single-title">
+                        <div class="icon">
+                            <span class="icon-data-guard-sentinel"></span>
+                        </div>
+                        <h3><a href="ai-lnsights.html">نگهبان داده‌ها</a></h3>
+                    </div>
+                    <div class="services-one__single-content">
+                        <p>این یک واقعیت ثابت شده است که یک خواننده ممکن است با محتوای قابل خواندن یک صفحه، که به طور طولانی تثبیت شده است، حواسش پرت شود.</p>
+                        <div class="btn-box">
+                            <a class="button-style-2" href="ai-lnsights.html">
+                                بیشتر بخوانید
+                                <i class="icon-plus-1"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--End Services One Single -->
+            <!--Start Services One Single -->
+            <div class="col-xl-4 col-lg-4 wow animated fadeInUp" data-wow-delay="0.5s">
+                <div class="services-one__single">
+                    <div class="services-one__single-title">
+                        <div class="icon">
+                            <span class="icon-code-genius-hub"></span>
+                        </div>
+                        <h3><a href="ai-lnsights.html">اپلیکیشن سوئیفت دِوآپ</a></h3>
+                    </div>
+                    <div class="services-one__single-content">
+                        <p>این یک واقعیت ثابت شده است که یک خواننده ممکن است با محتوای قابل خواندن یک صفحه، که به طور طولانی تثبیت شده است، حواسش پرت شود.</p>
+                        <div class="btn-box">
+                            <a class="button-style-2" href="ai-lnsights.html">
+                                بیشتر بخوانید
+                                <i class="icon-plus-1"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+/*
+import  "@/rtl/assets/js/vendor/jquery-3.6.0.min.js"
+import  "@/rtl/assets/js/vendor/jquery-sidebar-content.js"
+import  "@/rtl/assets/js/bootstrap.min.js"
+import  "@/rtl/assets/js/isotope.pkgd.min.js"
+import  "@/rtl/assets/js/imagesloaded.pkgd.min.js"
+import  "@/rtl/assets/js/jquery.odometer.min.js"
+import  "@/rtl/assets/js/jquery.appear.js"
+import  "@/rtl/assets/js/ajax-form.js"
+import  "@/rtl/assets/js/wow.min.js"
+import  "@/rtl/assets/js/isotope.js"
+import  "@/rtl/assets/js/jquery.magnific-popup.min.js"
+import  "@/rtl/assets/js/gsap.js"
+import  "@/rtl/assets/js/ScrollTrigger.js"
+import  "@/rtl/assets/js/SplitText.js"
+import  "@/rtl/assets/js/main.js"
+*/
+
+</script>
+
--- main/mainPage/index.vue
+++ main/mainPage/index.vue
@@ -2,6 +2,7 @@
   <div dir="rtl">
     <Part1/>
     <Part2/>
+    <Part3/>
   </div>
 </template>
 
@@ -9,4 +10,5 @@
 
 import Part1 from "~/main/mainPage/components/Part1.vue";
 import Part2 from "~/main/mainPage/components/Part2.vue";
+import Part3 from "~/main/mainPage/components/part3.vue";
 </script>
